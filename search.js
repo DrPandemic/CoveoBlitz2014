a6ca6e01607eefae96f9fdde2b2759354ea54218
Jesse Emond <emond.jesse@gmail.com>
2014-01-11 08:29:50 -0500

    + prevent query before commit

--- search.js
+++ search.js
@@ -2,14 +2,22 @@ function Search() {
   this.dic = {};
 
   this.index = function(doc) {
+    console.log('Indexing.');
   };
 
   this.commit = function() {
+    console.log('Commit');
+
+    this.query = do_query;
   };
 
-  this.search = function(query) {
-    
+  this.query = function() {
+    console.error('Must commit first.');
   };
+
+  function do_query(query) {
+    console.log('Query');
+  }
 }
 
 module.exports = Search;
--- test_search.js
+++ test_search.js
@@ -1,2 +1,6 @@
 ﻿var Search = require('./search');
 var search = new Search();
+
+search.index('Test');
+search.commit();
+search.query('yolo');
